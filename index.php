c8ede7668cdb8fee933b9e788eb0ee640f47f981
Vinayak Malladad <vmalladad@cpapi-sandbox-ukapi-m03.ihost.aol.com>
2016-12-01 23:56:47 -0500

    sss

--- index.php
+++ index.php
@@ -55,7 +55,7 @@ if(preg_match('[time|current time|now]', strtolower($message))) {
 			}';
 				
 	}else{
-		$message = $input['entry'][0]['messaging'][0]['message']['quick_replies'];
+		$message = $input['entry'][0]['messaging'][0]['message'];
     $jsonData = '{
     "data":"'.$message.'"		
     "recipient":{
